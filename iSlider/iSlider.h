f6901ad13bc764ce1178cfd4a987fd96ebaf5fc6
ahmed <>
2021-09-06 00:34:36 -0400

    Initial project setup

---
+++ iSlider/iSlider.h
@@ -0,0 +1,18 @@
+//
+//  iSlider.h
+//  iSlider
+//
+//  Created by ahmed on 9/6/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iSlider.
+FOUNDATION_EXPORT double iSliderVersionNumber;
+
+//! Project version string for iSlider.
+FOUNDATION_EXPORT const unsigned char iSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iSlider/PublicHeader.h>
+
+
